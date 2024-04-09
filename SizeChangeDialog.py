164a0b17fa2e6692e9b5b0f47dcf41d4a28a41b7
ratmanC <portaltier3@gmail.com>
2024-04-09 17:57:07 +0900

    Update SizeChangeDialog.py
    Changed some def and variable name
    Changed comment out for apply() and get_size()

--- SizeChangeDialog.py
+++ SizeChangeDialog.py
@@ -7,23 +7,23 @@ import re
 class SizeChangeDialog(simpledialog.Dialog):
 
     # 相方となるEntryと入力後になる予定の文字列が両方存在しているときにのみOKボタンを有効化する
-    def verticalEntrySwitchOKButtonValidate(self, editedstr):
+    def judgeSwitchOKButtonByVerticalEntry(self, editedstr):
         if self.horizon_entry.get() and editedstr:
             self.button1["state"] = tk.NORMAL
         else:
             self.button1["state"] = tk.DISABLED
-        return self.entryValidate(editedstr)
+        return self.validateEntry(editedstr)
 
-    def horizonEntrySwitchOKButtonValidate(self, editedstr):
-        if self.vertical_entry.get() and editedstr:
+    def judgeSwitchOKButtonByHorizonEntry(self, edited_str):
+        if self.vertical_entry.get() and edited_str:
             self.button1["state"] = tk.NORMAL
         else:
             self.button1["state"] = tk.DISABLED
-        return self.entryValidate(editedstr)
+        return self.validateEntry(edited_str)
 
     # 数字の入力と文字の削除しか受け付けないようにする
-    def entryValidate(self, editedstr):
-        if re.fullmatch(re.compile("[0-9]+"), editedstr) or not editedstr:
+    def validateEntry(self, edited_str):
+        if re.fullmatch(re.compile("[0-9]+"), edited_str) or not edited_str:
             return True
         else:
             # 入力をキャンセルする際、入力前の状態でOKボタンの状態を再度決定する
@@ -38,8 +38,8 @@ class SizeChangeDialog(simpledialog.Dialog):
         super().__init__(parent=master, title="サイズ変更")
 
     def body(self, master):
-        vertical_val_cmd = self.register(self.verticalEntrySwitchOKButtonValidate)
-        horizon_val_cmd = self.register(self.horizonEntrySwitchOKButtonValidate)
+        vertical_val_cmd = self.register(self.judgeSwitchOKButtonByVerticalEntry)
+        horizon_val_cmd = self.register(self.judgeSwitchOKButtonByHorizonEntry)
         self.vertical_entry: tk.Entry = tk.Entry(master, width=10, validate="key",
                                                  validatecommand=(vertical_val_cmd, "%P"))
         self.horizon_entry: tk.Entry = tk.Entry(master, width=10, validate="key",
@@ -57,7 +57,7 @@ class SizeChangeDialog(simpledialog.Dialog):
 
         box.pack()
 
-    # 戻り値の設定
+    # get_sizeで呼び出されたときのみ戻り値を返し、OKボタンで閉じた場合のみ戻り値の中身が設定されている
     def apply(self):
         self.size = int(self.vertical_entry.get()), int(self.horizon_entry.get())
 
